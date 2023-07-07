c1508a8b962af105b7b8f3c0cd4ea1b923926274
Manuel Duarte  <carlosd99m@gmail.com>
2023-07-06 22:45:24 -0600

    project architecture

---
+++ lib/presentation/bloc/weather_bloc.dart
@@ -0,0 +1,13 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'weather_event.dart';
+part 'weather_state.dart';
+
+class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
+  WeatherBloc() : super(WeatherInitial()) {
+    on<WeatherEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
