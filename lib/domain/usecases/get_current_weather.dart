0d165ef1e478c78d6ad942326b91ce9dd46b6b39
Manuel Duarte  <carlosd99m@gmail.com>
2023-07-28 16:29:48 -0600

    add weather domain layer

---
+++ lib/domain/usecases/get_current_weather.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:weather_app/data/failure.dart';
+import 'package:weather_app/domain/entities/weather.dart';
+import 'package:weather_app/domain/repositories/weather_repository.dart';
+
+class GetCurrentWeather {
+  final WeatherRepository repository;
+
+  GetCurrentWeather(this.repository);
+
+  Future<Either<Failure, Weather>> call(String cityName) {
+    return repository.getCurrentWeather(cityName);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:weather_app/app/weather_app.dart';
 
-void main() {
-  runApp(const WeatherApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  /// Flutter DotEnv
+  await dotenv.load();
+
+  runZonedGuarded(() {
+    runApp(const WeatherApp());
+  }, (error, stackTrace) {
+    print('runZonedGuarded: Caught error in my root zone.');
+  });
 }
