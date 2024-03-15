ce18cf830f1bf7a8d465595f53ed8d3c502d2365
Manuel Duarte  <carlosd99m@gmail.com>
2024-03-15 02:22:25 -0600

    Add new images and icons, update README.md and gitignore (#4)

---
+++ ios/weather_homescreen/weather_homescreenBundle.swift
@@ -0,0 +1,10 @@
+import WidgetKit
+import SwiftUI
+
+@main
+struct weather_homescreenBundle: WidgetBundle {
+    var body: some Widget {
+        weather_homescreen()
+        weather_homescreenLiveActivity()
+    }
+}
--- lib/domain/repositories/weather_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dartz/dartz.dart';
-
-import 'package:weather_app/data/failure.dart';
-import 'package:weather_app/domain/entities/weather.dart';
-
-abstract class WeatherRepository {
-  Future<Either<Failure, Weather>> getCurrentWeather(String cityName);
-}
